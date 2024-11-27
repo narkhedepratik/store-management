8fcdb100afb1acfcd6e62666b2f7eed4931f19f1
narkhedepratik <98662996+narkhedepratik@users.noreply.github.com>
2024-11-27 10:21:33 +0530

    crud operation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './template/Header';
+import AddProduct from './product/AddProduct';
+import ViewProducts from './product/ViewProducts';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <BrowserRouter>
+          <Header/>
+          <div className='background'>
+          <Routes>
+              <Route path="/" element={<ViewProducts/>}></Route>
+              <Route path="add" element={<AddProduct/>}></Route>
+              <Route path="view" element={<ViewProducts/>}></Route>
+                        {/* pathvariable */}
+              <Route path='edit/:id' element={<AddProduct/>}></Route>
+          </Routes>
+          </div>
+       </BrowserRouter>
     </div>
   );
 }
